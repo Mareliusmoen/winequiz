490910475e4afc34497cfef5406bf95d7f145311
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-11 10:48:55 +0200

    add functionality to questions and answers buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
 const startButton = document.getElementById('start-button')
 const welcomeText = document.getElementById('welcome-txt')
 const questionContainerElement = document.getElementById('game-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-console.log('started')
-startButton.classList.add('hide')
-welcomeText.classList.add('hide')
-shuffledQuestions = questions.sort(() => Math.random() - .5)
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
+    console.log('started')
+    startButton.classList.add('hide')
+    welcomeText.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
-
+showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
+function showQuestion(question)
 
 function selectAnswer() {
 
@@ -25,8 +30,8 @@ const questions = [
     {
         question: 'What is the main grape variety in Prosecco wines?',
         answers: [
-            {text: 'Glera', correct: true },
-            {text: 'Chardonnay', correct: false },
+            { text: 'Glera', correct: true },
+            { text: 'Chardonnay', correct: false },
         ]
     }
 ]
