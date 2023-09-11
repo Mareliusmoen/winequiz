5d471b81b0d95e71573539976a19d94c552253a8
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-11 10:42:53 +0200

    add question randomization in startgame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const startButton = document.getElementById('start-button')
 const welcomeText = document.getElementById('welcome-txt')
 const questionContainerElement = document.getElementById('game-container')
+const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
@@ -8,6 +9,7 @@ function startGame() {
 console.log('started')
 startButton.classList.add('hide')
 welcomeText.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
 questionContainerElement.classList.remove('hide')
 setNextQuestion()
 }
@@ -19,3 +21,12 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+const questions = [
+    {
+        question: 'What is the main grape variety in Prosecco wines?',
+        answers: [
+            {text: 'Glera', correct: true },
+            {text: 'Chardonnay', correct: false },
+        ]
+    }
+]
